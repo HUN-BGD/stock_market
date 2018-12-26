c4a51a5780985fab4019f002e925cef30141c584
HUN-BGD <bgd2@freemail.hu>
2018-12-26 13:21:43 +0100

    stock_market now can create dir structure

--- stock_markets.py
+++ stock_markets.py
@@ -2,7 +2,7 @@ import os
 
 
 class StockMarket():
-    def __init__(self, data_root='.'):
+    def __init__(self, data_root='data'):
         self.name = 'NASDAQ'
         self.data_root = data_root
 
@@ -11,18 +11,18 @@ class StockMarket():
         print(self.data_root)
         print(os.listdir(self.data_root))
 
-    def get_stock_folder(self, stock):
-        return self.data_root + self.name + '/' + stock + '/'
+    def _get_stock_folder(self, stock):
+        return os.path.join(self.data_root, self.name, stock)
 
+    def _download_stock_data(self, stock, start_year, end_year):
+        dst_folder = self._get_stock_folder(stock)
+        print(dst_folder)
+        if not os.path.exists(dst_folder):
+            os.makedirs(dst_folder)
+        for year in range(start_year, end_year+1):
+            file_name = stock + '_' + str(year) + '.csv'
+            print(os.path.join(dst_folder, file_name))
 
 if __name__ == "__main__":
-    nasdaq = StockMarket('c://')
-    print(nasdaq.name)
-
-    "data_root/name/stickername"
-
-    nasdaq.list_folder()
-    nasdaq.get_stock_folder('ATVI')
-    vmi = nasdaq.get_stock_folder('ATVI')
-
-    print(vmi)
+    nasdaq = StockMarket()
+    nasdaq._download_stock_data('ATVI', 2015, 2018)
